6f285f948dd5521d4002ae9cb99c296d36f1351e
johnsonsyleung <johnsonsyleung@gmail.com>
2026-01-09 21:40:11 -0800

    added docstring to handel_missing.py

---
+++ src/dsci_524_group23/handle_missing.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def handel_missing(df, strategy='mean'):
+    """
+    Handles missing data in a pandas DataFrame.
+
+    Function returns a pandas DataFrame where missing
+    values are handled in a user-defined way.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Input DataFrame
+
+    strategy : str, default 'mean'
+        The strategy to use for handling missing values.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe where missing values have been handled.
+
+    Raises
+    ------
+    TypeError
+        If `df` is not a pandas DataFrame.
+
+    ValueError
+        If `strategy` is not a permitted strategy.
+    """
+    pass
